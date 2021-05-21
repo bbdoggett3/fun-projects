077935221c6f575ad0c494243d5fdb6499bef6db
bbdoggett3 <bbdoggett3@gmail.com>
2021-05-20 22:42:51 -0600

    finished problem 11

--- Javascript 2/array-practice.js
+++ Javascript 2/array-practice.js
@@ -289,7 +289,18 @@ for(var i = 0; i < num2; i++){
 */
 
 //Code Here
+function longer(arr1, arr2) {
+  for(let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if(arr1[i] > arr2[j]) {
+        return arr1;
+      } else {
+        return arr2;
+      }
+    }
+  }
 
+}
 
 
 /*
