5cdf3fa406e3755c8c2d18c2ae9512edeabcb1bd
bbdoggett3 <bbdoggett3@gmail.com>
2021-05-23 17:06:41 -0600

    setting up the javascript

---
+++ Cover Your Assests/script.js
@@ -0,0 +1,12 @@
+//Here I have created variables for each one of my buttons
+let decreaseBtn = document.querySelector(".btn-decrease");
+let increaseBtn = document.querySelector(".btn-increase");
+let counter = document.querySelector(".counter");
+
+decreaseBtn.addEventListener('click', ()=> {
+    console.log('decrease')
+});
+
+increaseBtn.addEventListener('click', ()=> {
+    console.log('increase')
+});
